20f9778522a8ab062fefc057eff4da2aa72c1b18
Chris Leicester <cjleicester@googlemail.com>
2011-06-03 13:06:22 +0100

    Initial Commit

---
+++ MoneyMatters/MoneyMattersAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  MoneyMattersAppDelegate.h
+//  MoneyMatters
+//
+//  Created by Chris Leicester on 03/06/2011.
+//  Copyright 2011 CocoaShed Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoneyMattersAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ MoneyMatters/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MoneyMatters
+//
+//  Created by Chris Leicester on 03/06/2011.
+//  Copyright 2011 CocoaShed Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
